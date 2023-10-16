e4cba24ca1c2adb68bf41cecae3d9ec32a634a14
faizee-infobeans <faizee.bano@infobeans.com>
2023-10-16 11:23:10 +0530

    babel configuration and and build folder

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CartState", {
+  enumerable: true,
+  get: function get() {
+    return _Context.CartState;
+  }
+});
+Object.defineProperty(exports, "Context", {
+  enumerable: true,
+  get: function get() {
+    return _Context.Context;
+  }
+});
+var _Context = require("./Context");
